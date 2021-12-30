86d7590ab152c60336d02f9e4b10e7044ffecbee
Milad Rostami <milad01.dev@gmail.com>
2021-12-30 23:46:19 +0330

    updated

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import { createContext, useState } from 'react/cjs/react.development'
+import React, { createContext, useState } from 'react'
 
 export const favContext = createContext({
   favorite:[],
