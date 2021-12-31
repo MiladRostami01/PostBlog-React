345742db23c421e6f00a14044c7d552880189ecf
Milad Rostami <milad01.dev@gmail.com>
2021-12-31 09:49:20 +0330

    updated

--- src/pages/AllPosts.js
+++ src/pages/AllPosts.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import { useEffect, useState } from 'react/cjs/react.development'
+import React, { useEffect, useState } from 'react'
 import PostList from '../components/posts/PostList'
 
 // const DUMMY_DATA = [
